70677e7548cec3ab5f536adc70d49df8403029cf
Richard <richard.jovelin@oicr.on.ca>
2016-07-20 17:56:50 -0400

    compute number of target sites for genes with short 3'UTRs

--- PlotTargetSitesShort3UTRsCNVnonCNV.py
+++ PlotTargetSitesShort3UTRsCNVnonCNV.py
@@ -35,6 +35,7 @@ domain = sys.argv[1]
 print(domain)
 # get the minimum 3'UTR length
 L = int(sys.argv[2])
+assert L in [15, 7], 'minimum 3UTR length is not correct'
 print(L)
 
 # use all chromos (including unplaced, unlocated, and MT) or only valid chromos 
@@ -49,8 +50,6 @@ cnv_length = 'CNV_all_length'
 # make a dictionary of species names : species code
 species_codes = {'H_sapiens': 'Hsa', 'P_troglodytes': 'Ptr', 'M_mulatta': 'Mml',
                  'M_musculus': 'Mmu', 'B_taurus': 'Bta', 'G_gallus':'Gga'}
-
-#######################
 
 # make a dictionnary {species: {gene: [N_targets, Sequence_length, N_targets_normalized, CNV_status}}
 # for each predictor, targetscan and miranda
@@ -86,6 +85,14 @@ for i in range(len(predictors)):
             CNV_file = species + '_' + cnv_length + '_' + chromos + '.txt' 
         print(CNV_file)
         
+        # get CNV gene status
+        CNV_status = sort_genes_CNV_status(CNV_file)
+        print('CNV status', len(CNV_status))
+        
+        # add CNV status
+        for gene in targets:
+            targets[gene].append(CNV_status[gene])
+            
         # get file with UTR status (short, long)
         UTR_file = species + '_3UTR_length_' + chromos + '.txt'
         print(UTR_file)
@@ -107,103 +114,369 @@ for i in range(len(predictors)):
         infile.close()                    
         print('UTR length', len(UTR_length))
         
-
-################# continue here        
+        # initialize inner dict
+        if i == 0:
+            targetscan[species] = {}
+            for gene in targets:
+                # record gene with short 3'UTR
+                if gene in UTR_length and UTR_length[gene] == 'short':
+                    targetscan[species][gene] = list(targets[gene])
+        elif i == 1:
+            miranda[species] = {}
+            for gene in targets:
+                # record gene with short 3'UTR
+                if gene in UTR_length and UTR_length[gene] == 'short':
+                    miranda[species][gene] = list(targets[gene])
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
-        # get CNV gene status
-        CNV_status = sort_genes_CNV_status(CNV_file)
-        print('CNV status', len(CNV_status))
-    
-        # write number of target sites, sequence length, normalized targets and CNV status to file
-        newfile = open(outputfile, 'w')
-        # write header
-        newfile.write('\t'.join(['Gene', 'N_targets', 'Sequence_length', 'N_targets_normalized', 'CNV_status']) + '\n')
-    
-        # loop over genes in targets
-        for gene in targets:
-            # record only genes with short 3'UTR
-            if gene in UTR_length and UTR_length[gene] == 'short':
-                # write number of sites, sequence length and normalized number of targets to file
-                newfile.write('\t'.join([gene, str(targets[gene][0]), str(targets[gene][1]), str(targets[gene][2])]) + '\t')
-                # write CNV status
-                newfile.write(CNV_status[gene] + '\n')
-        # close file after writing
-        newfile.close()
-        print('done writing targets to file')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##########################
+# check that the same number of species are recorded for miranda and targetscan
+assert len(targetscan) == len(miranda), 'different number of species depending on predictor'
+# check that list values have the correct number of items
+for species in targetscan:
+    for gene in targetscan[species]:
+        assert len(targetscan[species][gene]) == 4, 'gene in targetscan does not have all required values'
+    for gene  in miranda[species]:
+        assert len(miranda[species][gene]) == 4, 'gene in miranda does not have all required values'
 
 
 # create a dict of {species name : [[CNV], [non-CNV]]}
-species_data = {}
+SpeciesDataTargetscan, SpeciesDataMiranda = {}, {}
 
-# loop over species names
-# parse the summary target file, extract number of normalized sites and CNV status
-# populate dict, use dict to generate figure        
+# loop over species names, get the number of normalized sites for CNV and non-CNV genes
+for species in targetscan:
+    # initialise list value
+    SpeciesDataTargetscan[species] = [[], []]
+    # populate inner lists with number of miRNA target sites per nucleotide
+    for gene in targetscan:
+        if targetscan[species][gene][-1] == 'CNV':
+            SpeciesDataTargetscan[species][0].append(targetscan[species][gene][2])
+        elif targetscan[species][gene][-1] == 'not_CNV':
+            SpeciesDataTargetscan[species][1].append(targetscan[species][gene][2])
+for species in miranda:
+    # initialize list values
+    SpeciesDataMiranda[species] = [[], []]
+    # populate inner lists with number of mirna target sites per nucleotide
+    for gene in miranda:
+        if miranda[species][gene][-1] == 'CNV':
+            SpeciesDataMiranda[species][0].append(miranda[species][gene][2])
+        elif miranda[species][gene][-1] == 'not_CNV':
+            SpeciesDataMiranda[species][1].append(miranda[species][gene][2])
+print('generated lists of target sites for CNV and non-CNV genes')
+
+
+# perform stattistical tests between CNV and non-CNV genes
+# create a dict to store results {species: P-value}
+CompTargetscan, CompMiranda = {}, {}
+for species in SpeciesDataTargetscan:
+    P = stats.ranksums(SpeciesDataTargetscan[species][0], SpeciesDataTargetscan[species][1])[1]
+    CompTargetscan[species] = P
+for species in SpeciesDataMiranda:
+    P = stats.ranksums(SpeciesDataMiranda[species][0], SpeciesDataMiranda[species][1])[1]    
+    CompMiranda[species] = P
+print('compared CNV and non-CNV genes')
+
+# print P-values
+for species in CompTargetscan:
+    print('targetscan', species, CompTargetscan[species])
+for species in CompMiranda:
+    print('miranda', species, CompMiranda[species])
+
+
+# make a list of data for each predictor
+AllDataTargetscan, AllDataMiranda = [], []
+
+# make a list of species names to loop from
+species_names = ['H_sapiens', 'P_troglodytes', 'M_mulatta', 'M_musculus', 'B_taurus', 'G_gallus']
+# loop over species in species names list and populate data lists, keeping the same order for targetscan and miranda
 for species in species_names:
-    print(species)
-    
-    # get the summary target file
-    if species == 'H_sapiens':
-        # use DGV 2015 release 
-        summary_file = species + '_short3UTRgenes_' + domain + '_summary_' + predictor + '_' + chromos + '_' + cnv_length + '_GRCh37_2015.txt'
-    else:
-        summary_file = species + '_short3UTRgenes_' + domain + '_summary_' + predictor + '_' + chromos + '_' + cnv_length + '.txt'
-    
-    # parse the summary table into a list of list of values
-    # [CNV_targets, nonCNV_targets, CNV_seq, nonCNV_seq, CNV_normalized, nonCNV_normalized]    
-    parsed_data = parse_summary_table_targets(summary_file)
-    
-    # populate dicts with normalized target sites
-    species_data[species] = [parsed_data[4], parsed_data[5]]
-    print(species, len(species_data[species][0]), len(species_data[species][1]))
-        
-print('nb species', len(species_data))    
-    
-# make a list of data
-all_data = []
-# loop over species name, append list of CNV values and list of non-CNV values
-for species in species_names:
-    all_data.append(species_data[species][0])
-    all_data.append(species_data[species][1])     
+    # append list of target sites for CNV genes
+    AllDataTargetscan.append(SpeciesDataTargetscan[species][0])
+    AllDataMiranda.append(SpeciesDataMiranda[species][0])
+    # append list of target sites for non-CNV genes
+    AllDataTargetscan.append(SpeciesDataTargetscan[species][1])
+    AllDataMiranda.append(SpeciesDataMiranda[species][1])
 print('data consolidated in array')
-    
 
 
 # create figure
-fig = plt.figure(1, figsize = (4.3,2.56))
+fig = plt.figure(1, figsize = (5, 2.5))
 
-# add a plot to figure (1 row, 1 column, 1 plot)
-ax = fig.add_subplot(1, 1, 1)    
+    
+# create a function to format the subplots
+def CreateAx(Columns, Rows, Position, Xscale, Data, figure, Title, XLabel = False, YLabel = False):
+    # create subplot in figure
+    # add a plot to figure (N row, N column, plot N)
+    ax = figure.add_subplot(Rows, Columns, Position)
+    # create a list of positions for the box plot    
+    BoxPositions = [0, 0.6, 0.8, 1.4, 1.6, 2.2, 2.4, 3, 3.2, 3.8, 4, 4.6]
+    # use a boxplot
+    bp = ax.boxplot(Data, showmeans = True, showfliers = False, widths = 0.5,
+                    positions = BoxPositions, patch_artist = True) 
+    
+    
+          
+    
+    
+    
+    
+    
+    
+    
+    # color CNV boxes in grey
+    i = 0    
+    # change box, whisker color to black
+    for box in bp['boxes']:
+        # change line color
+        box.set(color = 'black')
+        if i % 2 == 0:
+            # CNV data, color box in grey
+            box.set(facecolor = "grey")
+        else:
+            box.set(facecolor = 'white')
+        i += 1
+        
+    # change whisker color to black
+    for wk in bp['whiskers']:
+        wk.set(color = 'black', linestyle = '-')
+    
+    # change color of the caps
+    for cap in bp['caps']:
+        cap.set(color = 'black')
+        
+    # change the color and line width of the medians
+    for median in bp['medians']:
+        median.set(color = 'black')
+        
+    # change the mean marker and marker
+    for mean in bp['means']:
+        mean.set(marker = 'o', markeredgecolor = 'black', markerfacecolor = 'black', markersize = 4)
+    
+    
+    
+    
+    
+    ax.set_title(Title, size = 10)
+    
+    
+    
+    
+    
+    # write label for y axis
+    ytext = ax.set_ylabel('Normalized number of miRNA\nsites per gene', color = 'grey',  size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
+
+
+
+
+
+
+
+
+
+
+# set tick label
+names = []
+for species in species_names:
+    names.append(species_codes[species] + '_CNV')
+    names.append(species_codes[species] + '_non_CNV')
+
+# add labels to x-ticks, rotate and align right, set size to 14
+ax.set_xticklabels(names, rotation = 30, ha = 'right', size = 10, fontname = 'Arial', family = 'sans-serif')
+
+
+# add a light grey horizontal grid to the plot, semi-transparent, 
+ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)  
+
+# hide these grids behind plot objects
+ax.set_axisbelow(True)
+
+
+
+    
+    
+
+# do not show lines around figure  
+ax.spines["top"].set_visible(False)    
+ax.spines["bottom"].set_visible(False)    
+ax.spines["right"].set_visible(False)    
+ax.spines["left"].set_visible(False)  
+
+# create a list with range of x-axis values
+xvals = [i + 0.5 for i in range(len(names) + 1)]
+# Set a buffer around the edge of the x-axis
+plt.xlim([min(xvals)- 0.5, max(xvals)+ 0.5])
+
+# do not show ticks
+plt.tick_params(
+    axis='y',       # changes apply to the x-axis and y-axis (other option : x, y)
+    which='both',      # both major and minor ticks are affected
+    bottom='off',      # ticks along the bottom edge are off
+    top='off',         # ticks along the top edge are off
+    right = 'off',
+    left = 'off',          
+    labelbottom='off', # labels along the bottom edge are off 
+    colors = 'grey',
+    labelsize = 10)
+      
+
+# do not show ticks
+plt.tick_params(
+    axis='x',       # changes apply to the x-axis and y-axis (other option : x, y)
+    which='both',      # both major and minor ticks are affected
+    bottom='off',      # ticks along the bottom edge are off
+    top='off',         # ticks along the top edge are off
+    right = 'off',
+    left = 'off',          
+    labelbottom='on', # labels along the bottom edge are on 
+    colors = 'black',
+    labelsize = 10)
+
+# Set the tick labels font name
+for label in ax.get_yticklabels():
+    label.set_fontname('Arial')
+
+# add title
+if predictor == 'targetscan':
+    plt.title('TargetScan', size = 10, fontname = 'Arial')  
+elif predictor == 'miranda':
+    plt.title('miRanda', size = 10, fontname = 'Arial')
+
+
+# get outputfile
+outputfile = 'Fig_short3UTR_CNVvsNonCNV_' + domain + '_' + chromos + '_' + cnv_length + '_' + predictor + '_normalized_sites' 
+print(outputfile)
+
+# save figure
+fig.savefig(outputfile + '.eps', bbox_inches = 'tight')
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if YLabel == True:
+        # set y axis label
+        ax.set_ylabel('Protein pairs', size = 7, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
+    if XLabel == True:
+        # set x axis label
+        ax.set_xlabel('Protein distance', size = 7, ha = 'center', fontname = 'Helvetica', family = 'sans-serif')
+
+    # do not show lines around figure, keep bottow line  
+    ax.spines["top"].set_visible(False)    
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False)      
+    
+    if BottomLine == True:
+        ax.spines["bottom"].set_visible(True)
+    else:
+        ax.spines["bottom"].set_visible(False)
+    
+        # add a light grey horizontal grid to the plot, semi-transparent, 
+    ax.yaxis.grid(True, linestyle=':', which='major', color='lightgrey', alpha=0.5, linewidth = 0.5)  
+    # hide these grids behind plot objects
+    ax.set_axisbelow(True)    
+        
+    if BottomLine == True:
+        # do not show ticks
+        plt.tick_params(
+            axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
+            which='both',      # both major and minor ticks are affected
+            bottom='on',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            right = 'off',
+            left = 'off',          
+            labelbottom='on', # labels along the bottom edge are on
+            colors = 'black',
+            labelsize = 7,
+            direction = 'out') # ticks are outside the frame when bottom = 'on'  
+    else:
+        # do not show ticks
+        plt.tick_params(
+            axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
+            which='both',      # both major and minor ticks are affected
+            bottom='off',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            right = 'off',
+            left = 'off',          
+            labelbottom='off', # labels along the bottom edge are on
+            colors = 'black',
+            labelsize = 7,
+            direction = 'out') # ticks are outside the frame when bottom = 'on'  
+
+    if XLabel == True:
+        # determine tick position on x axis
+        xpos =  [i /10  for i in range(11)]
+        Dist = ['0', '', '', '', '', '0.5', '', '', '', '', '1']
+        # set up tick positions and labels
+        plt.xticks(xpos, Dist, rotation = 0, fontsize = 7, ha = 'center', fontname = 'Helvetica')
+
+    if YLabel == True:
+        plt.yticks(fontsize = 8)
+  
+    return ax
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# set width for all subplots    
+width = 0.1
+
+ax1 = CreateAx(4, 5, 1, [i / 10 for i in range(10)], Distances[families[0]], fig, families[0], width, YLabel = True)
+ax2 = CreateAx(4, 5, 2, [i / 10 for i in range(10)], Distances[families[1]], fig, families[1], width)
+ax3 = CreateAx(4, 5, 3, [i / 10 for i in range(10)], Distances[families[2]], fig, families[2], width)
+ax4 = CreateAx(4, 5, 4, [i / 10 for i in range(10)], Distances[families[3]], fig, families[3], width)
+ax5 = CreateAx(4, 5, 5, [i / 10 for i in range(10)], Distances[families[4]], fig, families[4], width, YLabel = True)
+ax6 = CreateAx(4, 5, 6, [i / 10 for i in range(10)], Distances[families[5]], fig, families[5], width)
+ax7 = CreateAx(4, 5, 7, [i / 10 for i in range(10)], Distances[families[6]], fig, families[6], width)
+ax8 = CreateAx(4, 5, 8, [i / 10 for i in range(10)], Distances[families[7]], fig, families[7], width)
+ax9 = CreateAx(4, 5, 9, [i / 10 for i in range(10)], Distances[families[8]], fig, families[8], width, YLabel = True)
+ax10 = CreateAx(4, 5, 10, [i / 10 for i in range(10)], Distances[families[9]], fig, families[9], width)
+ax11 = CreateAx(4, 5, 11, [i / 10 for i in range(10)], Distances[families[10]], fig, families[10], width)
+ax12 = CreateAx(4, 5, 12, [i / 10 for i in range(10)], Distances[families[11]], fig, families[11], width)
+ax13 = CreateAx(4, 5, 13, [i / 10 for i in range(10)], Distances[families[12]], fig, families[12], width, YLabel = True)
+ax14 = CreateAx(4, 5, 14, [i / 10 for i in range(10)], Distances[families[13]], fig, families[13], width)
+ax15 = CreateAx(4, 5, 15, [i / 10 for i in range(10)], Distances[families[14]], fig, families[14], width)
+ax16 = CreateAx(4, 5, 16, [i / 10 for i in range(10)], Distances[families[15]], fig, families[15], width)
+ax17 = CreateAx(4, 5, 17, [i / 10 for i in range(10)], Distances[families[16]], fig, families[16], width, XLabel = True, YLabel = True, BottomLine = True)
+ax18 = CreateAx(4, 5, 18, [i / 10 for i in range(10)], Distances[families[17]], fig, families[17], width, XLabel = True, BottomLine = True)
+ax19 = CreateAx(4, 5, 19, [i / 10 for i in range(10)], Distances[families[18]], fig, families[18], width, XLabel = True, BottomLine = True)
+
+# make sure subplots do not overlap
+plt.tight_layout()
+
+# save figure
+fig.savefig('ChemoProtDivergenceFamilies.pdf', bbox_inches = 'tight')
+
+
+#################
+
+
+
+
 
 # write label for y axis
 ytext = ax.set_ylabel('Normalized number of miRNA\nsites per gene', color = 'grey',  size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
