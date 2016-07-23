ff9411a8cdfa2f1b0b024f06c1417ec6d527ba9c
Richard <richard.jovelin@oicr.on.ca>
2016-07-23 19:26:56 -0400

    plot the number of short 3'UTR genes

--- PlotNumberShort3UTRGenesDVG.py
+++ PlotNumberShort3UTRGenesDVG.py
@@ -94,536 +94,84 @@ for filename in CNV_files:
 print('got short gene counts for each species')
 
 
+# plot the number of genes with short 3'UTR in each release of the DVG as a bar graph
 
-
-
-
-#################
-
-
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 12 23:45:20 2015
-
-@author: Richard
-"""
-
-
-
-
-# plot the number of studies with ratio of CNV / non-CNV short 3'UTR genes
-# for each release of the DGV
-
-# usage plot_CNVnonCNV_ratio_DGV.py [parameters]
-# [True/False] use valid chromos or all chromos
-# [all_CNVs/long_CNVs] use all CNVs or CNVs > 1Kb
-
-
-import os
-import sys
-import matplotlib.pyplot as plt
-
-# get chromos from command
-keep_valid_chromos = sys.argv[1]
-if keep_valid_chromos == 'True':
-    chromos = 'valid_chromos'
-elif keep_valid_chromos == 'False':
-    chromos = 'all_chromos'
+Releases = ['GRCh37_2013-05', 'GRCh37_2013-07', 'GRCh37_2014', 'GRCh37_2015']
+labelnames = ['2013a', '2013b', '2014', '2015']
     
-# get the type of CNVs to consider from the command [long_CNVs or all_CNVs]
-CNV = sys.argv[2]
-if CNV == 'all_CNVs':
-    cnv_length = 'CNV_all_length'
-elif CNV == 'long_CNVs':
-    cnv_length = 'CNV_greater_1Kb'
+# create list of counts for cnv genes and non-CNV genes parallel to labelnames list
+cnv_genes, non_cnv_genes = [], []
 
+for i in Releases:
+    cnv_genes.append(ShortGenes[i][1])
+    non_cnv_genes.append(ShortGenes[i][2])
 
-# make a list of files with number of genes with short 3' UTR
-# in CNV and non-CNV for each version of DGV
+# create figure
+fig = plt.figure(1, figsize = (4.3,2.56))
 
-files = [i for i in os.listdir() if ('Human_Counts_Short3UTR_' + cnv_length + '_' + chromos) in i]
+# add axe to fig
+ax = fig.add_subplot(1, 1, 1)
 
-#create a dict with version and list of counts
-ratio = {}
-for filename in files:
-    # grab version
-    version = filename[filename.rindex('_')+1: filename.index('.txt')]
-    # open file for reading
-    infile = open(filename, 'r')
-    # skip 2 first lines
-    infile.readline()
-    infile.readline()
-    # create a list of 0
-    counts = [0] * 10
-    # loop over file
-    for line in infile:
-        line = line.rstrip()
-        if line != '':
-            line = line.split('\t')
-            cnv = int(line[3])
-            noncnv = int(line[4])
-            freq = cnv / noncnv
-            # get the index in list where value should go
-            pos = int((freq * 100) // 10)
-            counts[pos] += 1
-    # close file
-    infile.close()
-    # populate dict with version : empty list pairs
-    ratio[version] = counts
-    
-# ~1.33x wider than tall
-# Common sizes: (10, 7.5) and (12, 9)    
-plt.figure(figsize=(8, 5.5))    
-  
-# Remove the plot frame lines.     
-ax = plt.subplot(111)    
+# Set the bar width
+bar_width = 0.5
+
+# set positions of the x-axis ticks
+xtickpos = [0, 0.7, 1.4, 2.1]
+
+# Create a bar plot for cnv genes
+ax.bar(xtickpos, cnv_genes, width=bar_width, label = 'CNV', color= '#ef8a62')
+# Create a bar plot for non_cnv genes on top of cnv_genes
+ax.bar(xtickpos, non_cnv_genes, width=bar_width, bottom= cnv_genes, label = 'non-CNV', color = '#67a9cf')
+
+# set font for all text in figure
+FigFont = {'fontname':'Arial'} 
+
+# add labels to x axis ticks
+plt.xticks(xtickpos, labelnames, **FigFont)
+
+# set axis labels
+ax.set_ylabel('Number of genes\nwith short 3\'UTR', size = 10, ha = 'center', color = 'black', **FigFont)
+
+# do not show lines around figure  
 ax.spines["top"].set_visible(False)    
 ax.spines["bottom"].set_visible(True)    
 ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(True)    
-
-# find the maximum y value
-maximum = 0
-for year in ratio:
-    for val in ratio[year]:
-        if val > maximum:
-            maximum = val
-
-# Limit the range of the plot to data
-plt.ylim(0, maximum)    
-plt.xlim(0, 10)    
-  
-# adjust size of ticks    
-  # set major ticks on the y axis
-plt.yticks(range(0, maximum + 10, 10), fontname = 'Arial', fontsize=16)    
-plt.xticks([i for i in range(0, 11)], fontname = 'Arial', fontsize=16)    
-  
-# add a light grey horizontal grid to the plot, semi-transparent, 
-ax.yaxis.grid(True, linestyle='-', which='major', color='0.80', alpha=0.6)  
-# hide these grids behind plot objects
-ax.set_axisbelow(True)
-
-# Remove the tick marks; they are unnecessary with the tick lines we just plotted.    
-plt.tick_params(axis="both", which="both", bottom="on", top="off",    
-                labelbottom="on", left="on", right="off", labelleft="on")    
-
-# plot the data
-
-releases = ['2013a', '2013b', '2014', '2015']
-
-# make a list of years
-years = ['2013-05', '2013-07', '2014', '2015']
-
-for year in years:
-    if year == '2013-05':
-        plt.plot([i + 0.5 for i in range(0, 10)], ratio[year], linestyle = '-', color = '0.80', marker = 'o', markersize = 10, markeredgewidth = 2, markerfacecolor = '0.80', markeredgecolor = '0.80', lw = 3, label = '2013a')
-    elif year == '2013-07':
-        plt.plot([i + 0.5 for i in range(0, 10)], ratio[year], linestyle = '-', color = '0.60', marker = 'o', markersize = 10, markeredgewidth = 2, markerfacecolor = '0.60', markeredgecolor = '0.60',   lw = 3, label = '2013b')
-    elif year == '2014':
-        plt.plot([i + 0.5 for i in range(0, 10)], ratio[year], linestyle = '-', color = '0.40', marker = 'o', markersize = 10, markeredgewidth = 2, markerfacecolor = '0.40', markeredgecolor = '0.40', lw = 3, label = '2014')
-    elif year == '2015':
-        plt.plot([i + 0.5 for i in range(0, 10)], ratio[year], linestyle = '-', color = '0.20', marker = 'o', markersize = 10, markeredgewidth = 2, markerfacecolor = '0.20', markeredgecolor = '0.20', lw = 3, label = '2015')
-
-# add x and y labels
-plt.xlabel('Ratio of number of CNV genes / non-CNV genes', fontname = 'Arial', fontsize = 16)
-plt.ylabel('Number of studies in DGV', fontname = 'Arial', fontsize = 16)
-
-
-# do not show ticks
-plt.tick_params(
-    axis='y',       # changes apply to the x-axis and y-axis (other option : x, y)
-    which='both',      # both major and minor ticks are affected
-    bottom='off',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    right = 'off',
-    left = 'off',          
-    labelbottom='off', # labels along the bottom edge are off 
-    colors = 'grey'
-    )  
-
-# do not show lines around figure  
-ax.spines["top"].set_visible(False)    
-ax.spines["bottom"].set_visible(False)    
-ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(False) 
-
-# get maximum y value
-ymax = 0
-for year in ratio:
-    if max(ratio[year]) > ymax:
-        ymax = max(ratio[year])
-        print(ymax)
-
-# Set a buffer around the edge
-plt.ylim([-1, round(ymax, -1)])
-
-
-# bbox_inches="tight" removes all the extra whitespace on the edges of the plot    
-plt.savefig("Fig_ratio_CNVnonCNV_DGV.eps", bbox_inches="tight")  
-
-  
-
-
-
-
-
-
-
-
-
-
-#####################
-
-
-
-
-
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov  8 10:18:44 2015
-
-@author: Richard
-"""
-
-
-# plot the number of genes with short 3'UTR in each species or DVG release as a bar graph
-
-
-# usage python3 plot_number_short3UTR_genes.py options
-# [species/human] number of genes for each species (DVG-2015 for human) or for each DGV release
-# [True/False] valid_chromos or all chromos
-# [long_CNVs/all_CNVs] CNV > 1 Kb or all CNVs
-
-
-import sys
-import matplotlib.pyplot as plt
-from matplotlib import patches as mpatches
-import numpy as np
-
-# get the option to consider species or DGV
-species = sys.argv[1]
-print(species)
-
-# get the option to keep genes on all chromos (False) or only on assembled nuclear chromosomes (True) 
-keep_valid_chromos = sys.argv[2]
-if keep_valid_chromos == 'True':
-    chromos = 'valid_chromos'
-elif keep_valid_chromos == 'False':
-    chromos = 'all_chromos'
-print(keep_valid_chromos, chromos)
-
-# get the type of CNVs to consider from the command [long_CNVs or all_CNVs]
-long_CNV = sys.argv[3]
-if long_CNV == 'all_CNVs':
-    cnv_length = 'CNV_all_length'
-elif long_CNV == 'long_CNVs':
-    cnv_length = 'CNV_greater_1Kb'
-print(long_CNV, cnv_length)
-
-# get the input file with gene counts
-if species == 'species':
-    counts_file = 'Gene_Counts_Short_3UTR_' + cnv_length + '_' + chromos + '.txt'
-elif species == 'human':
-    counts_file = 'Gene_Counts_DGV_release_short_3UTR_' + cnv_length + '_' + chromos + '.txt'
-print(counts_file)
-
-
-# open file for reading
-infile = open(counts_file, 'r')
-# skip 2 first lines
-infile.readline()
-infile.readline()
-
-# create a dict to store the {species/DVG_release :[total, cnv, non-cnv]} gene counts
-gene_counts = {}
-for line in infile:
-    line = line.rstrip()
-    if line != '':
-        line = line.split()
-        gene_counts[line[0]] = [int(line[1]), int(line[2]), int(line[3])]
-# close file after reading
-infile.close()
-
-
-# make a list of species names or release versions
-if species == 'species':
-    names = ['H_sapiens', 'P_troglodytes', 'M_mulatta', 'M_musculus', 'B_taurus', 'G_gallus']
-    labelnames = ['Hsa', 'Ptr', 'Mml', 'Mmu', 'Bta', 'Gga']
-elif species == 'human':
-    names = ['GRCh37_2013-05', 'GRCh37_2013-07', 'GRCh37_2014', 'GRCh37_2015']
-    labelnames = ['2013a', '2013b', '2014', '2015']
-    
-
-
-# create list of counts for cnv genes parallel to labelnames list
-cnv_genes = []
-# create a list of counts for non-cnv genes parallel to labelnames list
-non_cnv_genes = []
-
-# loop over species in name
-for sp in names:
-    cnv_genes.append(gene_counts[sp][1])
-    non_cnv_genes.append(gene_counts[sp][2])
-    
-# create figure
-fig = plt.figure(1, figsize = (4.3,2.56))
-
-# add axe to fig
-ax = fig.add_subplot(1, 1, 1)
-
-# Set the bar width
-bar_width = 0.75
-
-# set positions of the left bar-boundaries
-bar_left = [i+1 for i in range(len(cnv_genes))]
-
-# set positions of the x-axis ticks (center of the bars as bar labels)
-tick_pos = [i+(bar_width/2) for i in bar_left]
-
-# Create a bar plot, in position bar_left for cnv genes
-ax.bar(bar_left, cnv_genes, width=bar_width, label = 'CNV', color= 'black')
-
-# Create a bar plot, in position bar_left for non_cnv genes on top of cnv_genes
-ax.bar(bar_left, non_cnv_genes, width=bar_width, bottom= cnv_genes, label = 'non-CNV', color = 'white')
-
-# set the x ticks with names
-if species == 'species':
-    plt.xticks(tick_pos, labelnames, style = 'italic')
-elif species == 'human':
-    plt.xticks(tick_pos, labelnames)
-
-# set axis labels
-ax.set_ylabel('Number of genes\nwith short 3\'UTR', size = 12, ha = 'center', fontname = 'Arial', family = 'sans-serif', color = 'grey')
-
-# check if species is species or human
-if species == 'human':
-    # add a x-axis label
-    ax.set_xlabel('DGV releases', size = 12, ha = 'center', fontname = 'Arial', family = 'sans-serif')
-elif species == 'species':
-    # add a x-axis label
-    ax.set_xlabel('Vertebrate species', size = 12, ha = 'center', fontname = 'Arial', family = 'sans-serif')
-
-
-# Set a buffer around the edge
-plt.xlim([min(tick_pos)-bar_width, max(tick_pos)+bar_width])
-
-
+ax.spines["left"].set_visible(False)  
+# offset the spines
+for spine in ax.spines.values():
+    spine.set_position(('outward', 5))
 
 # add a light grey horizontal grid to the plot, semi-transparent, 
 ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)  
 # hide these grids behind plot objects
 ax.set_axisbelow(True)
 
-# do not show lines around figure  
-ax.spines["top"].set_visible(False)    
-ax.spines["bottom"].set_visible(False)    
-ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(False)  
-
-
 # do not show ticks
-  
 plt.tick_params(
-    axis='y',       # changes apply to the x-axis and y-axis (other option : x, y)
+    axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
     which='both',      # both major and minor ticks are affected
-    bottom='off',      # ticks along the bottom edge are off
+    bottom='on',      # ticks along the bottom edge are off
     top='off',         # ticks along the top edge are off
     right = 'off',
     left = 'off',          
-    labelbottom='off', # labels along the bottom edge are off 
-    colors = 'grey')  
-
-
-
-#
-# remove top axes and right axes ticks
-ax.get_xaxis().tick_bottom()
-#ax.get_yaxis().tick_left()
+    labelbottom='on', # labels along the bottom edge are on
+    colors = 'black',
+    labelsize = 10,
+    direction = 'out') # ticks are outside the frame when bottom = 'on'  
+      
+# Set the tick labels font name
+for label in ax.get_yticklabels():
+    label.set_fontname('Arial')
+    
+# create a margin around the x axis
+plt.margins(0.05)
 
 # get outputfile
-outputfile = 'Fig_short3UTR_counts_' + '_' + species + '_' + cnv_length + '_' + chromos
+if L == 7:
+    outputfile = 'PlotShort3UTRCountsDGV_7bp_' + cnv_length + '_' + chromos
+elif L == 15:
+    outputfile = 'PlotShort3UTRCountsDGV_15bp_' + cnv_length + '_' + chromos
 print(outputfile)
   
 # save figure
 fig.savefig(outputfile + '.eps', bbox_inches = 'tight')
-    
-    
-    
-    
-########################
-    
-    
-    
-    
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov  8 10:18:44 2015
-
-@author: Richard
-"""
-
-
-# plot the number of genes with short 3'UTR in each species or DVG release as a bar graph
-
-
-# usage python3 plot_number_short3UTR_genes.py options
-# [species/human] number of genes for each species (DVG-2015 for human) or for each DGV release
-# [True/False] valid_chromos or all chromos
-# [long_CNVs/all_CNVs] CNV > 1 Kb or all CNVs
-
-
-
-import os
-import sys
-import matplotlib.pyplot as plt
-from matplotlib import patches as mpatches
-import numpy as np
-
-# get the option to consider species or DGV
-species = sys.argv[1]
-print(species)
-
-# get the option to keep genes on all chromos (False) or only on assembled nuclear chromosomes (True) 
-keep_valid_chromos = sys.argv[2]
-if keep_valid_chromos == 'True':
-    chromos = 'valid_chromos'
-elif keep_valid_chromos == 'False':
-    chromos = 'all_chromos'
-print(keep_valid_chromos, chromos)
-
-# get the type of CNVs to consider from the command [long_CNVs or all_CNVs]
-long_CNV = sys.argv[3]
-if long_CNV == 'all_CNVs':
-    cnv_length = 'CNV_all_length'
-elif long_CNV == 'long_CNVs':
-    cnv_length = 'CNV_greater_1Kb'
-print(long_CNV, cnv_length)
-
-# get the input file with gene counts
-if species == 'species':
-    counts_file = 'Gene_Counts_Short_3UTR_' + cnv_length + '_' + chromos + '.txt'
-elif species == 'human':
-    counts_file = 'Gene_Counts_DGV_release_short_3UTR_' + cnv_length + '_' + chromos + '.txt'
-print(counts_file)
-
-
-# open file for reading
-infile = open(counts_file, 'r')
-# skip 2 first lines
-infile.readline()
-infile.readline()
-
-# create a dict to store the {species/DVG_release :[total, cnv, non-cnv]} gene counts
-gene_counts = {}
-for line in infile:
-    line = line.rstrip()
-    if line != '':
-        line = line.split()
-        gene_counts[line[0]] = [int(line[1]), int(line[2]), int(line[3])]
-# close file after reading
-infile.close()
-
-
-# make a list of species names or release versions
-if species == 'species':
-    names = ['H_sapiens', 'P_troglodytes', 'M_mulatta', 'M_musculus', 'B_taurus', 'G_gallus']
-    labelnames = ['Hsa', 'Ptr', 'Mml', 'Mmu', 'Bta', 'Gga']
-elif species == 'human':
-    names = ['GRCh37_2013-05', 'GRCh37_2013-07', 'GRCh37_2014', 'GRCh37_2015']
-    labelnames = ['2013a', '2013b', '2014', '2015']
-    
-
-
-# create list of counts for cnv genes parallel to labelnames list
-cnv_genes = []
-# create a list of counts for non-cnv genes parallel to labelnames list
-non_cnv_genes = []
-
-# loop over species in name
-for sp in names:
-    cnv_genes.append(gene_counts[sp][1])
-    non_cnv_genes.append(gene_counts[sp][2])
-    
-# create figure
-fig = plt.figure(1, figsize = (4.3,2.56))
-
-# add axe to fig
-ax = fig.add_subplot(1, 1, 1)
-
-# Set the bar width
-bar_width = 0.75
-
-# set positions of the left bar-boundaries
-bar_left = [i+1 for i in range(len(cnv_genes))]
-
-# set positions of the x-axis ticks (center of the bars as bar labels)
-tick_pos = [i+(bar_width/2) for i in bar_left]
-
-# Create a bar plot, in position bar_left for cnv genes
-ax.bar(bar_left, cnv_genes, width=bar_width, label = 'CNV', color= 'black')
-
-# Create a bar plot, in position bar_left for non_cnv genes on top of cnv_genes
-ax.bar(bar_left, non_cnv_genes, width=bar_width, bottom= cnv_genes, label = 'non-CNV', color = 'white')
-
-# set the x ticks with names
-if species == 'species':
-    plt.xticks(tick_pos, labelnames, style = 'italic')
-elif species == 'human':
-    plt.xticks(tick_pos, labelnames)
-
-# set axis labels
-ax.set_ylabel('Number of genes\nwith short 3\'UTR', size = 12, ha = 'center', fontname = 'Arial', family = 'sans-serif', color = 'grey')
-
-# check if species is species or human
-if species == 'human':
-    # add a x-axis label
-    ax.set_xlabel('DGV releases', size = 12, ha = 'center', fontname = 'Arial', family = 'sans-serif')
-elif species == 'species':
-    # add a x-axis label
-    ax.set_xlabel('Vertebrate species', size = 12, ha = 'center', fontname = 'Arial', family = 'sans-serif')
-
-
-# Set a buffer around the edge
-plt.xlim([min(tick_pos)-bar_width, max(tick_pos)+bar_width])
-
-
-
-# add a light grey horizontal grid to the plot, semi-transparent, 
-ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)  
-# hide these grids behind plot objects
-ax.set_axisbelow(True)
-
-# do not show lines around figure  
-ax.spines["top"].set_visible(False)    
-ax.spines["bottom"].set_visible(False)    
-ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(False)  
-
-
-# do not show ticks
-  
-plt.tick_params(
-    axis='y',       # changes apply to the x-axis and y-axis (other option : x, y)
-    which='both',      # both major and minor ticks are affected
-    bottom='off',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    right = 'off',
-    left = 'off',          
-    labelbottom='off', # labels along the bottom edge are off 
-    colors = 'grey')  
-
-
-
-#
-# remove top axes and right axes ticks
-ax.get_xaxis().tick_bottom()
-#ax.get_yaxis().tick_left()
-
-# get outputfile
-outputfile = 'Fig_short3UTR_counts_' + '_' + species + '_' + cnv_length + '_' + chromos
-print(outputfile)
-  
-# save figure
-fig.savefig(outputfile + '.eps', bbox_inches = 'tight')
-    
