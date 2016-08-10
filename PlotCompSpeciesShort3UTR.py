4f454535c91d61c42291555f88ac3d1ebe8e9ca9
Richard <richard.jovelin@oicr.on.ca>
2016-08-10 13:26:01 -0400

    plot target sites in human and other vertebrates

--- PlotCompSpeciesShort3UTR.py
+++ PlotCompSpeciesShort3UTR.py
@@ -135,154 +135,269 @@ for domain in ['5UTR', 'CDS']:
         HsaSpeciesTargets[SpeciesNames[i]][domain]['CNV'] = [Sp1TargetsCNV[:], Sp2TargetsCNV[:]]
         HsaSpeciesTargets[SpeciesNames[i]][domain]['not_CNV'] = [Sp1TargetsNonCNV[:], Sp2TargetsNonCNV[:]]
     
-        
-################################################
-    
+
 
 # plot the number of target sites of mirna families shared between human and
 # with each other species for CNV and for non-CNV genes
  
+# create paralell lists for target sites in CNV and non-CNV genes located in 5'UTR or CDS 
+CNVGenesCDS, CNVGenes5UTR, NonCNVGenesCDS, NonCNVGenes5UTR = [], [], [], [] 
+# loop over species names
+for species in SpeciesNames:
+    # loop over domain
+    for domain in HsaSpeciesTargets[species]:
+        # loop over CNV status
+        for status in HsaSpeciesTargets[species][domain]:
+            # check domain and CNV status
+            if domain == 'CDS' and status == 'CNV':
+                for i in range(2):
+                    CNVGenesCDS.append(HsaSpeciesTargets[species][domain][status][i])
+            elif domain == 'CDS' and status =='not_CNV':
+                for i in range(2):
+                    NonCNVGenesCDS.append(HsaSpeciesTargets[species][domain][status][i])
+            elif domain == '5UTR' and status == 'CNV':
+                for i in range(2):
+                    CNVGenes5UTR.append(HsaSpeciesTargets[species][domain][status][i])
+            elif domain == '5UTR' and status == 'not_CNV':
+                for i in range(2):
+                    NonCNVGenes5UTR.append(HsaSpeciesTargets[species][domain][status][i])
+print('generated lists of target sites for CNV and non-CNV genes')
 
-# make a list of species pairs
-species_pairs = ['Hsa_Ptr', 'Hsa_Mml', 'Hsa_Mmu', 'Hsa_Bta', 'Hsa_Gga']
+
+## perform stattistical tests between CNV and non-CNV genes
+## create dicts to store results {species: [P-value absolute targets, P-value normalized targets]}
+#CompTargetscan, CompMiranda = {}, {}
+#for species in SpeciesDataTargetscanAbsolute:
+#    Pabs = stats.ranksums(SpeciesDataTargetscanAbsolute[species][0], SpeciesDataTargetscanAbsolute[species][1])[1]
+#    Pnorm = stats.ranksums(SpeciesDataTargetscanNormalized[species][0], SpeciesDataTargetscanNormalized[species][1])[1]
+#    CompTargetscan[species] = [Pabs, Pnorm]
+#for species in SpeciesDataMirandaAbsolute:
+#    Pabs = stats.ranksums(SpeciesDataMirandaAbsolute[species][0], SpeciesDataMirandaAbsolute[species][1])[1]    
+#    Pnorm = stats.ranksums(SpeciesDataMirandaNormalized[species][0], SpeciesDataMirandaNormalized[species][1])[1]    
+#    CompMiranda[species] = [Pabs, Pnorm]
+#print('compared CNV and non-CNV genes')
+#
+## print P-values
+#for species in CompTargetscan:
+#    print('targetscan', species, CompTargetscan[species])
+#for species in CompMiranda:
+#    print('miranda', species, CompMiranda[species])
 
 
 
-
-
-
-# make a list of lists with all data
-all_data = []
-# loop over species pairs, append list of sites
-for i in range(len(species_pairs)):
-    all_data.append(species_data[species_pairs[i]][0])
-    all_data.append(species_data[species_pairs[i]][1])
-print('data consolidated in array')
-    
-    
 # create figure
-fig = plt.figure(1, figsize = (4.3,2.56))
+fig = plt.figure(1, figsize = (8, 5))
 
-# add a plot to figure (1 row, 1 column, 1 plot)
-ax = fig.add_subplot(1, 1, 1)    
+# create list of labels and tick positions for the X axis
+xtickpos = [0.2, 1.1, 2, 2.9, 3.8]
+Names = [species_codes[i] for i in SpeciesNames]
+print(Names)
 
-# write label for y axis
-ytext = ax.set_ylabel('Normalized number of miRNA\nsites per gene', color = 'grey', size = 10, ha = 'center', fontname = 'Arial', family = 'sans-serif')
-
-# set tick label
-names = []
-for i in range(len(species_pairs)):
-    names.extend(list(species_pairs[i].split('_')))
-
-
-# add labels to x-ticks, and align center, set size to 10
-ax.set_xticklabels(names, ha = 'center', size = 10, fontname = 'Arial', family = 'sans-serif')
-
-
-# add a light grey horizontal grid to the plot, semi-transparent, 
-ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)  
-# hide these grids behind plot objects
-ax.set_axisbelow(True)
-
-# remove top axes and right axes ticks
-ax.get_xaxis().tick_bottom()
-ax.get_yaxis().tick_left()
-
-
-# use a boxplot
-bp = ax.boxplot(all_data, showmeans = True, showfliers = False, widths = 0.7, labels = names, patch_artist = True) 
-  
- 
-# color CNV boxes in grey
-i = 0    
-# change box, whisker color to black
-for box in bp['boxes']:
-    # change line color
-    box.set(color = 'black')
-    if i % 2 == 0:
-        # CNV data, color box in grey
-        box.set(facecolor = "grey")
-    else:
-        box.set(facecolor = 'white')
-    i += 1
-        
-# change whisker color ro black
-for wk in bp['whiskers']:
-    wk.set(color = 'black', linestyle = '-')
+# create a function to format the subplots
+def CreateAx(Columns, Rows, Position, Data, figure, Title, SpeciesNames, XScale):
+    '''
+    (int, int, int, list, figure_object, str, int, list, list)
+    Take the number of a column, and rows in the figure object and the position of
+    the ax in figure, a list of data, a title, a maximum value for the Y axis,
+    a list with species names and list of X axis tick positions and return an
+    ax instance in the figure
+    '''    
     
-# change color of the caps
-for cap in bp['caps']:
-    cap.set(color = 'black')
-        
-# change the color and line width of the medians
-for median in bp['medians']:
-    median.set(color = 'black')
-        
-# change the mean marker and marker
-for mean in bp['means']:
-    mean.set(marker = 'o', markeredgecolor = 'black', markerfacecolor = 'black', markersize = 4)
     
+    # create subplot in figure
+    # add a plot to figure (N row, N column, plot N)
+    ax = figure.add_subplot(Rows, Columns, Position)
+    # create a list of positions for the box plot    
+    BoxPositions = [0, 0.4, 0.9, 1.3, 1.8, 2.2, 2.7, 3.1, 3.6, 4]
+    # use a boxplot
+    bp = ax.boxplot(Data, showmeans = True, showfliers = False, widths = 0.3,
+                    positions = BoxPositions, patch_artist = True) 
+    
+    # color CNV and non-CNV boxes differently
+    i = 0    
+    # change box, whisker color to black
+    for box in bp['boxes']:
+        # change line color
+        box.set(color = 'black')
+        if i % 2 == 0:
+            # CNV data, color box in grey
+            box.set(facecolor = '#a6cee3')
+        else:
+            box.set(facecolor = '#b2df8a')
+        i += 1
+    # change whisker color to black
+    for wk in bp['whiskers']:
+        wk.set(color = 'black', linestyle = '-')
+    # change color of the caps
+    for cap in bp['caps']:
+        cap.set(color = 'black')
+    # change the color and line width of the medians
+    for median in bp['medians']:
+        median.set(color = 'black')
+    # change the mean marker and marker
+    for mean in bp['means']:
+        mean.set(marker = 'o', markeredgecolor = 'black', markerfacecolor = 'black', markersize = 3)
+    
+    
+    # write title   
+    ax.set_title(Title, size = 8)
+    # set font for all text in figure
+    FigFont = {'fontname':'Arial'}   
+    # write label for y axis
+    ax.set_ylabel('Normalized number of miRNA\nsites per gene', color = 'black',  size = 8, ha = 'center', **FigFont)
+    # write label for x axis
+    plt.xticks(XScale, SpeciesNames, ha = 'center', fontsize = 8, **FigFont)
 
-# do not show lines around figure  
-ax.spines["top"].set_visible(False)    
-ax.spines["bottom"].set_visible(False)    
-ax.spines["right"].set_visible(False)    
-ax.spines["left"].set_visible(False)  
+    # add a range for the Y axis
+    plt.ylim([0, 0.16])
+    
+    plt.xlim([-0.25, 4.25])
+    
+    # do not show lines around figure  
+    ax.spines["top"].set_visible(False)    
+    ax.spines["bottom"].set_visible(True)    
+    ax.spines["right"].set_visible(False)    
+    ax.spines["left"].set_visible(False)  
+    # offset the spines
+    for spine in ax.spines.values():
+        spine.set_position(('outward', 5))
+    
+    # add a light grey horizontal grid to the plot, semi-transparent, 
+    ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5, linewidth = 0.5)  
+    # hide these grids behind plot objects
+    ax.set_axisbelow(True)
 
-
-# create a list with range of x-axis values
-xvals = [i + 0.5 for i in range(len(names) + 1)]
-# Set a buffer around the edge of the x-axis
-plt.xlim([min(xvals)- 0.5, max(xvals)+ 0.5])
-
-# do not show ticks
-plt.tick_params(
-    axis='y',       # changes apply to the x-axis and y-axis (other option : x, y)
-    which='both',      # both major and minor ticks are affected
-    bottom='off',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    right = 'off',
-    left = 'off',          
-    labelbottom='off', # labels along the bottom edge are off 
-    colors = 'grey',
-    labelsize = 10)
+    # do not show ticks
+    plt.tick_params(
+        axis='both',       # changes apply to the x-axis and y-axis (other option : x, y)
+        which='both',      # both major and minor ticks are affected
+        bottom='on',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        right = 'off',
+        left = 'off',          
+        labelbottom='on', # labels along the bottom edge are on
+        colors = 'black',
+        labelsize = 8,
+        direction = 'out') # ticks are outside the frame when bottom = 'on'  
       
-
-# do not show ticks
-plt.tick_params(
-    axis='x',       # changes apply to the x-axis and y-axis (other option : x, y)
-    which='both',      # both major and minor ticks are affected
-    bottom='off',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    right = 'off',
-    left = 'off',          
-    labelbottom='on', # labels along the bottom edge are on 
-    colors = 'black',
-    labelsize = 10)
-
-
-# Set the tick labels font name
-for label in ax.get_yticklabels():
-    label.set_fontname('Arial')
+    # Set the tick labels font name
+    for label in ax.get_yticklabels():
+        label.set_fontname('Arial')
     
-## note some ways to modify label font size and color of the ticks
-#for label in ax.get_yticklabels():
-#    label.set_color('black')
-#    label.set_fontsize(10)
+    # create a margin around the x axis
+    plt.margins(0.05)
+    
+    return ax      
 
-# add title
-if gene_type == 'CNV':
-    plt.title('CNV genes', size = 10, fontname = 'Arial')  
-elif gene_type == 'not_CNV':
-    plt.title('non-CNV genes', size = 10, fontname = 'Arial')
 
-# get outputfile
-outputfile = 'truc_' + domain + '_' + chromos + '_' + cnv_length + '_' + release_version + '_' + gene_type
-print(outputfile)
 
-  
+# create a lit of species abbrviations
+labelnames = [species_codes[i] for i in SpeciesNames]
+
+# plot box plots
+ax1 = CreateAx(2, 2, 1, CNVGenes5UTR, fig, '5\'UTRs of CNV genes', labelnames, xtickpos)
+ax2 = CreateAx(2, 2, 2, NonCNVGenes5UTR, fig, '5\'UTRs of non-CNV genes', labelnames, xtickpos)
+ax3 = CreateAx(2, 2, 3, CNVGenesCDS, fig, 'CDS of CNV genes', labelnames, xtickpos)
+ax4 = CreateAx(2, 2, 4, NonCNVGenesCDS, fig, 'CDS of non-CNV genes', labelnames, xtickpos)
+
+
+# add subplot labels
+ax1.text(-1.3, 1550, 'A', horizontalalignment = 'center', verticalalignment = 'center', color = 'black', size = 10)
+ax1.text(5.5, 1550, 'B', horizontalalignment = 'center', verticalalignment = 'center', color = 'black', size = 10)   
+ax3.text(-1.3, 2700, 'C', horizontalalignment = 'center', verticalalignment = 'center', color = 'black', size = 10)
+ax3.text(5.5, 2700, 'D', horizontalalignment = 'center', verticalalignment = 'center', color = 'black', size = 10) 
+
+
+## annotate Graph with significance level
+#PvalTargetScanAbsolute, PvalMirandaAbsolute, PvalTargetScanNormalized, PvalMirandaNormalized = [], [], [], []
+#for species in species_names:
+#    # get the significance level for target sites
+#    if CompTargetscan[species][0] >= 0.05:
+#        PvalTargetScanAbsolute.append('')
+#    elif CompTargetscan[species][0] < 0.05 and CompTargetscan[species][0] >= 0.01:
+#        PvalTargetScanAbsolute.append('*')
+#    elif CompTargetscan[species][0] < 0.01 and CompTargetscan[species][0] >= 0.001:
+#        PvalTargetScanAbsolute.append('**')
+#    elif CompTargetscan[species][0] < 0.001:
+#        PvalTargetScanAbsolute.append('***')
+#    # get the significance level of normalized target sites
+#    if CompTargetscan[species][1] >= 0.05:
+#        PvalTargetScanNormalized.append('')
+#    elif CompTargetscan[species][1] < 0.05 and CompTargetscan[species][1] >= 0.01:
+#        PvalTargetScanNormalized.append('*')
+#    elif CompTargetscan[species][1] < 0.01 and CompTargetscan[species][1] >= 0.001:
+#        PvalTargetScanNormalized.append('**')
+#    elif CompTargetscan[species][1] < 0.001:
+#        PvalTargetScanNormalized.append('***')
+#
+#for species in species_names:
+#    # get the significance level for target sites
+#    if CompMiranda[species][0] >= 0.05:
+#        PvalMirandaAbsolute.append('')
+#    elif CompMiranda[species][0] < 0.05 and CompMiranda[species][0] >= 0.01:
+#        PvalMirandaAbsolute.append('*')
+#    elif CompMiranda[species][0] < 0.01 and CompMiranda[species][0] >= 0.001:
+#        PvalMirandaAbsolute.append('**')
+#    elif CompMiranda[species][0] < 0.001:
+#        PvalMirandaAbsolute.append('***')
+#    # get the significance level of normalized target sites
+#    if CompMiranda[species][1] >= 0.05:
+#        PvalMirandaNormalized.append('')
+#    elif CompMiranda[species][1] < 0.05 and CompMiranda[species][1] >= 0.01:
+#        PvalMirandaNormalized.append('*')
+#    elif CompMiranda[species][1] < 0.01 and CompMiranda[species][1] >= 0.001:
+#        PvalMirandaNormalized.append('**')
+#    elif CompMiranda[species][1] < 0.001:
+#        PvalMirandaNormalized.append('***')
+#
+## create list of Y and X positions to annotate figure with significance level
+#if domain == '3UTR':
+#    # make a list of Y positions
+#    YposTargetscanAbsolute = [1390, 420, 400, 990, 390, 580]
+#    YposMirandaAbsolute = [820, 270, 210, 590, 220, 320]
+#    YposTargetscanNormalized = [0.42, 0.11, 0.16, 0.33, 0.15, 0.21]
+#    YposMirandaNormalized = [0.34, 0.10, 0.12, 0.23, 0.13, 0.16]
+#    Xpos = [0.2, 1.1, 2, 2.9, 3.8, 4.7]
+#elif domain == 'CDS':
+#    # make a list of Y positions
+#    YposTargetscanAbsolute = [1400, 420, 400, 1000, 390, 590]
+#    YposMirandaAbsolute = [820, 280, 210, 600, 220, 320]
+#    YposTargetscanNormalized = [0.42, 0.11, 0.16, 0.33, 0.16, 0.21]
+#    YposMirandaNormalized = [0.35, 0.10, 0.12, 0.24, 0.13, 0.16]
+#    Xpos = [0.2, 1.1, 2, 2.9, 3.8, 4.7]
+#elif domain == '5UTR':
+#    # make a list of Y positions
+#    YposTargetscanAbsolute = [4000, 1490, 2500, 2020, 1020, 610]
+#    YposMirandaAbsolute = [2520, 900, 1500, 1500, 300, 400]
+#    YposTargetscanNormalized = [0.40, 0.12, 0.16, 0.33, 0.15, 0.23]
+#    YposMirandaNormalized = [0.33, 0.09, 0.12, 0.23, 0.12, 0.17]
+#    Xpos = [0.2, 1.1, 2, 2.9, 3.8, 4.7]
+#
+#
+#for i in range(len(PvalTargetScanAbsolute)):
+#    ax1.text(Xpos[i], YposTargetscanAbsolute[i], PvalTargetScanAbsolute[i], horizontalalignment = 'center',
+#             verticalalignment = 'center', color = 'black', size = 8)
+#    ax2.text(Xpos[i], YposTargetscanNormalized[i], PvalTargetScanNormalized[i], horizontalalignment = 'center',
+#             verticalalignment = 'center', color = 'black', size = 8)         
+#for i in range(len(PvalMirandaAbsolute)):
+#    ax3.text(Xpos[i], YposMirandaAbsolute[i], PvalMirandaAbsolute[i], horizontalalignment = 'center',
+#             verticalalignment = 'center', color = 'black', size = 8)
+#    ax4.text(Xpos[i], YposMirandaNormalized[i], PvalMirandaNormalized[i], horizontalalignment = 'center',
+#             verticalalignment = 'center', color = 'black', size = 8)
+#
+## add legend relative to ax1 using ax1 coordinates
+#C = mpatches.Patch(facecolor = '#a6cee3', edgecolor = 'black', linewidth = 1, label= 'CNV')
+#N = mpatches.Patch(facecolor = '#b2df8a', edgecolor = 'black', linewidth = 1, label= 'non-CNV')
+#ax1.legend(handles = [C, N], loc = (0.8, 1.2), fontsize = 8, frameon = False, ncol = 2)
+
+
+# make sure subplots do not overlap
+plt.tight_layout()
+
+
+## build outputfile with arguments
+#outputfile = 'truc_' + domain + '_' + chromos + '_' + cnv_length
+#print(outputfile)
+
 # save figure
 fig.savefig('truc.pdf', bbox_inches = 'tight')
-
-
-
