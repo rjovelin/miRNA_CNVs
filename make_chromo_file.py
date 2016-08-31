15b0d402800f631cacdbbbefb7fcc59be779d5fa
Richard <richard.jovelin@oicr.on.ca>
2016-08-31 08:54:42 -0400

    find mRNAs affected by CNVR

--- make_chromo_file.py
+++ make_chromo_file.py
@@ -202,75 +202,6 @@ for line in infile:
 infile.close()
     
 
-
-
-# use this function to get the mRNA ID of each CDS ID
-def match_CDS_to_mRNA(GFF_file, valid_chromo_file, keep_valid_chromos):
-    '''
-    (file, file, bool) -> dict
-    Return a dictionary with CDS ID : mRNA ID pairs for all chromosomes
-    (including MT, unplaced and unlocalized) if keep_valid_chromos 
-    is False or for assembled nuclear chromosomes if True
-    '''
-    
-    # make a set of valid chromos
-    valid_chromos = get_valid_chromos(valid_chromo_file)
-    
-    # create dict {CDS ID : mRNA ID}
-    CDS = {}
-    
-    # open file for reading
-    infile = open(GFF_file, 'r')
-    # loop over file
-    for line in infile:
-        # find lines with CDS
-        if 'CDS' in line:
-            line = line.rstrip().split('\t')
-            # check that line corresponds to CDS info
-            if line[2] == 'CDS':
-                # get chromo
-                chromo = line[0]
-                # extract CDS ID
-                cds_id = line[-1][line[-1].index('ID=') + 3: line[-1].index(';')]
-                # extract parent mRNA ID
-                rna_id = line[-1][line[-1].index('Parent=') + 7: line[-1].index(';', line[-1].index('Parent='))]
-                # populate dict (each cds id maps to a single rna id)
-                # ignore genes that are not linked to a mRNA (they map to gene segment) 
-                if 'rna' in rna_id:
-                    # check if need to consider only valid chromos
-                    if keep_valid_chromos == True:
-                        # check that gene in valid chromo
-                        if chromo in valid_chromos:
-                            CDS[cds_id] = rna_id
-                    elif keep_valid_chromos == False:
-                        # record all genes, regardless of linkage
-                        CDS[cds_id] = rna_id                
-                
-    # close file
-    infile.close()
-    return CDS
-
-
-# use this function to map mRNA ID to their CDS id
-def match_mRNA_to_CDS(GFF_file, valid_chromo_file, keep_valid_chromos):
-    '''
-    (file, file, bool) -> dict
-    Return a dictionary with mRMA ID : CDS ID pairs for all chromosomes
-    (including MT, unplaced and unlocalized) if keep_valid_chromos 
-    is False or for assembled nuclear chromosomes if True
-    '''
-    
-    # get a dict with CDS ID : mRNA ID pairs
-    CDS = match_CDS_to_mRNA(GFF_file, valid_chromo_file, keep_valid_chromos)
-            
-    # reverse dict
-    mRNAs = {}
-    for cds_id in CDS:
-        mRNAs[CDS[cds_id]] = cds_id
-        
-    return mRNAs
-
-
 # get the coordinates of all mRNAs
 # create a dict {rna_id: [chromo, start, end , orientation]}
 mRNACoord = {}
@@ -297,75 +228,8 @@ for line in infile:
 infile.close()
     
 
- 
-    
-    # Note that a few rnas correspond to gene_ids that map to the same gene name
-    # keep a single rna corresponding to a single gene id that nap to a single gene name
-    # Rnor: 49; Hsa: 0; Ptr: 11; Cfa: 18; Bt: 94; Mmul: 335; Gga: 34; Mmus: 2
-
-    # create a set to store the longest mRNA for each gene (ie, a single mRNA per gene)
-    longest = set()
-    
-    # get the mRNA coordinates {mRNA : [chromo, start, end, orientation]}
-    mRNA_coord = get_mRNA_coord(GFF_file, valid_chromo_file, keep_valid_chromos)
-    # get the {mRNA ID : gene ID} pairs
-    mRNA_gene = match_mRNA_to_gene(GFF_file, valid_chromo_file, keep_valid_chromos)
-    # get the {gene ID: gene name} pairs
-    gene_names = geneID_to_gene_name(GFF_file, valid_chromo_file, keep_valid_chromos)
-    # get the {rna ID : CDS ID} pairs
-    mRNA_CDS = match_mRNA_to_CDS(GFF_file, valid_chromo_file, keep_valid_chromos)
-       
-    # compute the length of each mRNA
-    mRNA_length = {}
-    for rna in mRNA_coord:
-        L = mRNA_coord[rna][2] - mRNA_coord[rna][1]
-        mRNA_length[rna] = L
-    # create a dictionary with gene name as key : list of [L, rna]
-    genes = {}
-    for rna in mRNA_length:
-        # ignore rna_id not mapping to CDS ID
-        if rna in mRNA_CDS:
-            # get gene id
-            gene_id = mRNA_gene[rna]
-            # get gene name
-            name = gene_names[gene_id]
-            # populate dict
-            if name in genes:
-                # add [mRNA_length, mRNA]
-                genes[name].append([mRNA_length[rna], rna])
-            else:
-                genes[name] = [[mRNA_length[rna], rna]]
-            
-    # sort mRNA according to their length
-    for name in genes:
-        genes[name].sort()
-        # get the longest mRNA
-        rna = genes[name][-1][-1]
-        longest.add(rna)
-    
-    return longest
-        
-        
-    
-    longest = get_longest_mRNA(GFF_file, valid_chromo_file, keep_valid_chromos)
-        
-    # get the mRNA coordinates {rna_id: [chromo, start, end , orientation]}
-    mRNA_coord = get_mRNA_coord(GFF_file, valid_chromo_file, keep_valid_chromos)
-    
-    # get the CDS coordinates {cds_id: [chromo, orientation, [[start, end], [start, end]]}
-    CDS_coord = get_CDS_coord(GFF_file, valid_chromo_file, keep_valid_chromos)
-    
-    # get the cds_id of each rna_id {rna_id : cds id}
-    rna_to_cds = match_mRNA_to_CDS(GFF_file, valid_chromo_file, keep_valid_chromos)
-        
-    # get the gene id of each rna id {RNA ID : gene ID}
-    rna_to_gene = match_mRNA_to_gene(GFF_file, valid_chromo_file, keep_valid_chromos)
-    
-    # get the gene name for each gene id {gene ID : gene name pairs}
-    gene_names = geneID_to_gene_name(GFF_file, valid_chromo_file, keep_valid_chromos)
-        
-
-
-
-
+# find all mRNAs affected by CNVR
+for rna in mRNACoord:
+    # get mRNA coord
+    rna_chromo, rna_start, rna_end = mRNACoord[rna][0], mRNACoord[rna][1], mRNACoord[rna][2]
 
